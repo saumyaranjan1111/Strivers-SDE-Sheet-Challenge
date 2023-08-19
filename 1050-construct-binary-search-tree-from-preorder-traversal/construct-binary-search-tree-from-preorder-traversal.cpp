4935f787d012e92fd53387f1526e54e165a6def6
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2023-08-19 19:01:21 +0530

    Time: 6 ms (44.13%) | Memory: 13.8 MB (19.82%) - LeetSync

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* helper(vector<int>& preorder, int left, int right){
+        if(left > right) return NULL;
+        if(left <0 || right <0) return NULL;
+
+        TreeNode* root = new TreeNode(preorder[left]);
+        int pivot = -1;
+        int i = left+1;
+        while(i<=right && preorder[i] < root->val) i++;
+        pivot = i;
+
+        root->left = helper(preorder, left+1, pivot-1);
+        root->right = helper(preorder, pivot, right);
+
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int n = preorder.size();
+        if(n==0) return NULL;
+        int left = 0;
+        int right = n-1;
+        TreeNode* root = new TreeNode(preorder[left]);
+
+        int pivot = -1;
+
+        int i = left+1; 
+        while(i<=right && preorder[i] < root->val) i++;
+        pivot = i;
+        root->left = helper(preorder, left + 1, pivot-1);
+        root->right = helper(preorder, pivot, right);
+
+        return root;
+    }
+};
