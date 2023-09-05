0974e649c1a9a51b2d0c93656033ac892284d5ee
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2023-09-05 20:09:51 +0530

    Time: 2442 ms (19.97%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 
-select s.*, sub.subject_name, sum(case when e.student_id is null then 0 else 1 end) as attended_exams from Students as s 
+select s.*, sub.subject_name, count(e.student_id) as attended_exams 
+from Students as s 
 cross join Subjects as sub
 left join Examinations as e
 on s.student_id = e.student_id and sub.subject_name = e.subject_name
