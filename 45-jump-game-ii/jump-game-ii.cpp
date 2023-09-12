53ecd6b920383e5a942cc7c7395a75fc70cae662
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2023-09-12 17:41:37 +0530

    Time: 354 ms (22.41%) | Memory: 17.2 MB (31.63%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        
-        // state : dp[i] = min no of jumps required to reach n-1 from i
-        // transition : 
-        // dp[i] = infinity
-        // for 1<=jump<=nums[i]
-        // dp[i] = min(dp[i], 1 + dp[i+jump])
-        // base case: dp[n-1] = 0
-
-        // final subproblem : dp[0]
-
-        vector<int> dp(nums.size(), 1e8);
         int n = nums.size();
+        vector<int> dp(n, (int)1e8);
+
         dp[n-1] = 0;
         for(int i = n-2; i>=0; i--){
-            for(int jump = 1; jump<=nums[i]; jump++){
-                if(i + jump<=n-1)
-                dp[i] = min(dp[i], 1 + dp[i+jump]);
-                else break;
+            
+            for(int step = 1; step<= nums[i] && (i + step) < n; step++){
+                dp[i] = min(dp[i] , 1 + dp[i + step]);
             }
         }
         return dp[0];
