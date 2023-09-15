2bbb918382bcbea6ad3325e2f00e0216e5ebc9f3
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2023-09-15 21:06:39 +0530

    Time: 2 ms (57.00%) | Memory: 8.8 MB (11.43%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end());
-        vector<int> &a = citations;
-    
+        int ans = 0;
         int n = citations.size();
-
-        if(n == 1){
-            if(a[0] >= 1) return 1;
-            else return 0;
-        }
-        int hmax = 0;
-        for(int i = n-1; i>=0; i--){
-            if(n - i >= hmax){
-            hmax = max(hmax, min(a[i], n - i));
+        sort(citations.begin(), citations.end(), greater<int>());
+        int low = 0;
+        int high = n-1;
+        int mid;
+        // the question we will ask is can mid be a valid h-index, if yes then can it be better than the ones before it
+        while(low <= high){
+            mid = low + (high - low)/2;
+            if(citations[mid] >= mid+1) {
+                ans = max(ans, mid+1);
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
             }
         }
-        return hmax;
+        return ans;
     }
 };
