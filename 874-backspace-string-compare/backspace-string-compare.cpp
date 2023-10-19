ddbc341b5652aed27e0cb1af594f24a085f884a3
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2023-10-19 20:28:37 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (54.78%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,34 +1,67 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> st1;
-        for(int i = 0; i<s.size(); i++){
-            if(s[i] != '#'){
-                st1.push(s[i]);
-            }
-            else if(!st1.empty()){
-                st1.pop();
-            }
-        }
+        int n = s.size();
+        int m = t.size();
+        int count1 = 0;
+        int count2 = 0;
+        int i = n-1;
+        int j = m-1;
 
-        stack<char> st2;
-        for(int i = 0; i<t.size(); i++){
-            if(t[i] != '#'){
-                st2.push(t[i]);
-            }
-            else if(!st2.empty()){
-                st2.pop();
-            }
-        }
+        while(i>=0 || j>=0){
+            // run the outer loop until one of the strings becomes empty
 
-        while(!st1.empty() && !st2.empty()){
-            char one = st1.top();
-            char two = st2.top();
+            // delete all the characters until the first undeletable character
+            while(i>=0){
+                if(s[i] == '#'){
+                    // if the current char is a hash, then increase the no of characters that we still need to delete
+                    count1++;
+                    i--;
+                }
+                else{
+                    // if the current char is not a hash, then check if the count of characters that we need to delete is 0 or not, if the no is 0, then stop the loop , if not, then delete the curr char( reduce the no of characters we still need to delete )
+                    if(count1 > 0){
+                        count1--;
+                        i--;
+                    }
+                    else {
+                        break;
+                    }
 
-            if(one != two) return false;
-            st1.pop(); st2.pop();
+                }
+            }
+
+            while(j>=0){
+                if(t[j] == '#'){
+                    count2++;
+                    j--;
+                }
+                else{
+                    if(count2 > 0){
+                        count2--;
+                        j--;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+
+
+            // if any one of the strings becomes "empty" before the other, return false
+            if((i<0 && j>=0) || (j<0 && i>=0)){
+                return false;
+            }
+            
+            // after the running of the above two loops i and j are at the undeletable characters of both the strings
+
+            // if those two chars are not equal return false
+            if(i>=0 && j>=0 && s[i] != t[j]) return false;
+
+            // else go to the left character of both and run the loop again
+            i--;
+            j--;
         }
-        if(!st1.empty() || !st2.empty()) return false;
 
         return true;
     }
