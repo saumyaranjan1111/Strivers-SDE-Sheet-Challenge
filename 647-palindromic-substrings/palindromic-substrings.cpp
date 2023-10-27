5f83c8263d1385c0204018eb6ef19e1f611dd245
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2023-10-27 17:13:55 +0530

    Time: 4 ms (77.25%) | Memory: 6.8 MB (74.05%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
     int countSubstrings(string s) {
+        // expand from middle algo
+
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, false));
-        for(int i = 0; i<n; i++) dp[i][i] = true;
+        int count = 0;
 
-        for(int i = n-2; i>=0; i--){
-            for(int j = i+1; j<n; j++){
-                if(s[i] == s[j]){
-                    if(j == i+1) dp[i][j] = true;
-                    else
-                    dp[i][j] = dp[i+1][j-1];
-                }
-                else {
-                    dp[i][j] = false;
-                }
-            }
-        }
-
-        int total = 0;
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                total+=dp[i][j];
+            // i is the middle point, and we will find the length of the biggest palindrome that has i as its center
+
+            // now we can have our center in two ways
+            // way1: i is the center itself (odd palindrom) : "aba"
+            // way2: i,i+1 is the center (even palindrom) : "abba"
+
+            // way1:
+            int a = i, b = i;
+            while(a>=0 && b<n && s[a] == s[b]) {
+                a--;
+                b++;
             }
+            // b-a+1 is the length of the biggest palindrome of way1 from the center (i, i)
+            // ex : xyz has a length of 3, and contains 2 palindromes : xyx and y
+            // and r - l for this would be 4, hence count+= (r-l)/2
+            // so we increase the count by (r-l)/2
+
+            count+= (b-a)/2;
+
+            a = i, b = i+1;
+            while(a>=0 && b<n && s[a] == s[b]){
+                a--;
+                b++;
+            }
+            count+= (b-a)/2;
         }
-        return total;
+        return count;
     }
 };
