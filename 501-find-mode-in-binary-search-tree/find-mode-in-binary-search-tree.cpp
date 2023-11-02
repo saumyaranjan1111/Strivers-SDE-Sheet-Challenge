46daf68c638a50a460438ba8335ac80722c1b802
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2023-11-02 15:37:19 +0530

    Time: 14 ms (75.36%) | Memory: 24.8 MB (74.42%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
@@ -10,26 +10,51 @@
  * };
  */
 class Solution {
+    int maxcount = 0;
+    int currcount = 0;
+    unordered_set<int> st;
+    int prev = INT_MIN;
 private:
-    void count(TreeNode* node, unordered_map<int, int>& mp){
-        if(node == NULL) return;
-        mp[node->val]++;
-        count(node->left, mp);
-        count(node->right, mp);
+    void inorder(TreeNode* root, vector<int>& ans){
+        if(root == NULL) return;
+// the idea is that the inorder traversal of a bst traverses the nodes in ascending order
+
+// so if the current node is equal to the previous node in the preorder traversal, then increase the current count by 1
+// else reset the currcount to 1
+        
+        inorder(root->left, ans);
+
+        if(root->val == prev){
+            currcount++;
+        }
+        else {
+            currcount = 1;
+        }
+
+// now check if the current count is greater than the maxcount, if that is the case, then make maxcount equal to currcount, since this is the new maxcount, all of the elements previously inserted would be worthless now, so clear them
+// insert this new element into the set
+        if(currcount > maxcount){
+            maxcount = currcount;
+            st.clear();
+            st.insert(root->val);
+        }
+        else if(currcount == maxcount){
+            // if the current count is equal tot he maxcount, then this node value is also one of the modes until now, so insert it into the set
+            st.insert(root->val);
+        }
+
+        // set the prev to be the root val, so that , whatever node is processed afterwards knows that this was the last node processed before it (ascending order since this is a bst)
+        prev = root->val;
+
+        inorder(root->right, ans);
     }
 public:
     vector<int> findMode(TreeNode* root) {
-        unordered_map<int, int> mp;
-        count(root, mp);
-        int maxfreq = 0;
         vector<int> ans;
-        for(auto it = mp.begin(); it!= mp.end(); it++){
-            maxfreq = max(maxfreq, it->second);
-        }
-
-        for(auto it = mp.begin(); it!= mp.end(); it++){
-            if(it->second == maxfreq)
-            ans.push_back(it->first);
+        inorder(root, ans);
+        
+        for(auto it = st.begin(); it!= st.end(); it++){
+            ans.push_back(*it);
         }
         return ans;
     }
