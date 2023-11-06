420a5257c6f75522726f452593619ea49941fbde
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2023-11-06 10:04:16 +0530

    Time: 266 ms (94.83%) | Memory: 141.9 MB (96.99%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.cpp
+++ 1955-seat-reservation-manager/seat-reservation-manager.cpp
@@ -1,21 +1,35 @@
+// we can also solve this using a prepopulated set, but in that implementation, the set will contain all the n nodes that we might never need at a time and hence leads to a larger time complexity
+// so instead of storing the seats that were reserved in the set, we will store the seats that are unreserved in the pq and track the seats that were stored using a maxstored variable
+
 class SeatManager {
-private:
-    set<int> seats;
+
+// pq will contain all the unreserved seats
+
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int maxAllotedSeat = 0;
 public:
     SeatManager(int n) {
-        for(int i = 0; i<n; i++){
-            seats.insert(i+1);
-        }
     }
     
     int reserve() {
-        int firstseat = *seats.begin();
-        seats.erase(seats.begin());
-        return firstseat;
+        if(pq.empty()){
+            // pq is empty, so no seat is unreserved upto the value of maxAllotedSeat, so reserve the next seat
+            maxAllotedSeat++;
+            return maxAllotedSeat;
+        }
+        else{
+            // pq is not empty, so it contains some unreserved seats out of which the smallest one is at the top of the pq
+            // we return that seat and pop that seat from the pq as it is no longer unreserved.
+            int minUnreservedSeat = pq.top();
+            pq.pop();
+            return minUnreservedSeat;
+        }
+        return -1;
     }
     
     void unreserve(int seatNumber) {
-        seats.insert(seatNumber);
+        pq.push(seatNumber);
     }
 };
 
