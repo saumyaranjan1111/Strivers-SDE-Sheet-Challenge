7c7c3247a5d5247ec6bdc1660cdef88d1de427bf
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2023-11-06 10:32:58 +0530

    Time: 319 ms (54.55%) | Memory: 165.5 MB (9.09%) - LeetSync

--- 3191-maximum-score-after-applying-operations-on-a-tree/maximum-score-after-applying-operations-on-a-tree.cpp
+++ 3191-maximum-score-after-applying-operations-on-a-tree/maximum-score-after-applying-operations-on-a-tree.cpp
@@ -1,53 +1,58 @@
 class Solution {
 private:
-    long long dfs(int node, vector<vector<int>> &tree, int parent, vector<int>& values){
-
-        // if the current node is a leaf node
-        // assume that the leaf node is the minimum in that path for now and send it upwards, 
-        // if it not the minimum in that path, it will be taken care of up the line
-        if(tree[node].size()==1 && node!=0) 
-        {
-            // if the current node is a leafnode and it is not the root node, then the min value that we can keep in this 
+    long long sumofvaluestobeleftinthetree(int node, int parent, vector<vector<int>>& tree, vector<int>& values){
+        
+        if(tree[node].size() == 1 && parent != -1){
+            // if the no of children of a node is 1 and the parent is not -1, then it will be a leaf node
+            // we did not just use tree[node].size() == 1 because it can also apply to the case where the root node has just one child, and in that case our function will return the value of the root node without checking further down
+            // hence to specify that the node is a leaf node, we need to put the parent!=-1 condition
+            // this is a leaf node
+            // imagine what should this function return in the case where there is only one node which is this node, ofc we cant remove this node, as the one node in each path condition would be violated, so we just send the value of this node upwards
             return values[node];
         }
 
-        // go through all the children subtrees and find the sum of all the min nodes of the corresponding subtrees that we plan to leave in the final tree
-        // dfs(child) : gives the min node in the path from root->child->leaf
-        // so sumofsubtrees will contain the sum of all the min nodes that we plan to leave in the final subtree
-        // if we find that the sumofsubtrees ( i.e. the nodes below the current node that we plan to leave in the tree) < currentnodevalue ( that means that we can still send the sum of those nodes upwards )
-        // if we find that the sumofsubtrees > current node value ( we have found a better node to keep in the final subtree ) so we send this value upwards instead
-        long long sumofsubtrees = 0;
+        long long minvalsfromchildren = 0;
         for(auto child : tree[node]){
             if(child == parent) continue;
-            sumofsubtrees += dfs(child, tree, node, values);
+
+            minvalsfromchildren += sumofvaluestobeleftinthetree(child, node, tree, values);
+        }
+        if(minvalsfromchildren < values[node]){
+            // the minimum sum of all the nodes to be left in the tree is smaller than the value of the current node
+            // so we dont need to change anything, these are the nodes that need to be left in their respective subtrees in the final tree
+            // so just send this answer upwards
+            return minvalsfromchildren;
+        }
+        else{
+            // the value of the current node is smaller than the sum of all the nodes to be left in the final tree, in their respective subtrees
+            // so instead of leaving those nodes in the final subtree, we would prefer to leave this current node in the final subtree
+            return values[node];
         }
 
-        return min(sumofsubtrees, (long long)values[node]);
-
-    }
-    long long summ(int root, vector<vector<int>> &tree, int parent, vector<int>& values){
-        
-        long long sum = 0;
-        for(auto child : tree[root]){
-            if(child == parent) continue;
-            sum+= summ(child, tree, root, values);
-        }
-        sum += values[root];
-        return sum;
+        return -1;
     }
 public:
     long long maximumScoreAfterOperations(vector<vector<int>>& edges, vector<int>& values) {
         int n = values.size();
         vector<vector<int>> tree(n);
-        for(auto edge: edges){
+
+        for(auto edge : edges){
             int u = edge[0];
             int v = edge[1];
+
             tree[u].push_back(v);
             tree[v].push_back(u);
         }
-        long long minval = dfs(0, tree, -1, values);
-        long long totalsum = summ(0, tree, -1, values);
 
-        return totalsum - minval;
+        long long totalvalue = 0;
+        for(auto val : values){
+            totalvalue+=val;
+        }
+
+        long long sumofnodesleft = sumofvaluestobeleftinthetree(0, -1, tree, values);
+
+        long long score = totalvalue - sumofnodesleft;
+
+        return score;
     }
 };
