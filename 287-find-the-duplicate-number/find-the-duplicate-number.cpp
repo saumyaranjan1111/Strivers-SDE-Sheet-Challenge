74ba151b1f93c971479d8e47d14ce40f8cc22ede
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2023-12-13 19:32:07 +0530

    Time: 75 ms (86.64%) | Memory: 61.4 MB (88.95%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        
-        for(int i = 0; i<nums.size(); i++)
-        {
-            if(nums[abs(nums[i])] > 0)
-            {
-                nums[abs(nums[i])] = -nums[abs(nums[i])];
+        // index sort : if array is sorted then index i should contain the number i+1
+        // for every num that we come across , we will check its final position which will be num - 1, if num - 1 already has num, then we can say that num appears twice in the array
+        // if a num does not have another num present in the position num-1, then we swap, so as to make the array sorted upto that point and move forward
+        int n = nums.size();
+
+        for(int i = 0; i<n; ){
+            int num = nums[i];
+
+            if(num == i+1){
+                // num is present at the correct position and we should move on
+                i++;              
             }
-            else
-            {
-                return abs(nums[i]);
+            else if(nums[num-1] == num){
+                // duplicate found
+                return num;
+            }
+            else {
+                // neither num is present at its correct position, nor is it the duplicate( currently )
+                // put num in its correct position but stay at the same place and check for the new element that we brought in by swapping num to its correct position
+                swap(nums[num-1], nums[i]);
             }
         }
         return -1;
+
     }
 };
