1238bf5f4a7ada7f84c014e5bc879e8ece16c90e
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2024-01-02 18:06:37 +0530

    Time: 27 ms (5.95%) | Memory: 30.3 MB (45.69%) - LeetSync

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        map<int, int> freq;
+        int rows = 0;
+        for(auto &x : nums){
+            freq[x]++;
+        }
+
+        for(auto it = freq.begin(); it!= freq.end(); it++){
+            rows = max(rows, it->second);
+        }
+
+        while(!freq.empty()){
+            vector<int> temp;
+            for(auto it = freq.begin(); it!= freq.end(); it++){
+                if(it->second > 0){
+                    temp.push_back(it->first);
+                    it->second = it->second - 1;
+                }
+            }
+            if(temp.size() == 0) break;
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
