4a4373d6e05a9a191478bc985dba1e9a36ea6746
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2024-03-20 17:35:28 +0530

    Time: 47 ms (49.37%) | Memory: 51.3 MB (17.17%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,23 +1,19 @@
+using ll = long long;
 class Solution {
 public:
-    bool possible(int ind, vector<int>& nums, vector<int>& dp){
-        if(ind >= nums.size()) return false;
-        if(ind == nums.size() - 1) return true;
-
-        if(dp[ind] != -1) return dp[ind];
-
-        for(int i = 1; i<=nums[ind] ; i++){
-            if(possible(ind+i, nums, dp)) return dp[ind] = true;
-        }
-
-        return dp[ind] = false;
-    }
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-
-        if(possible(0, nums, dp)) return true;
-        
-        return false;
+        ll n = nums.size();
+        vector<bool> dp(n);
+        dp[n-1] = true;
+        ll lasttrue = n-1;
+        for(ll i = n-2; i>=0; i--){
+            if(lasttrue <= i+nums[i]){
+                dp[i] = true;
+                lasttrue = i;
+            } else {
+                dp[i] = false;
+            }
+        }
+        return dp[0];
     }
 };
