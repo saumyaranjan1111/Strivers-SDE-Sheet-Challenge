21899087c147652ff106647d50835a97eb579f8b
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2024-03-20 11:50:27 +0530

    Time: 296 ms (16.00%) | Memory: 71.9 MB (5.41%) - LeetSync

--- 2326-sum-of-scores-of-built-strings/sum-of-scores-of-built-strings.cpp
+++ 2326-sum-of-scores-of-built-strings/sum-of-scores-of-built-strings.cpp
@@ -1,36 +1,86 @@
+#define sz(x) ((int)(x).size())
 using ll = long long;
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll m){a = a % m; return expo(a, m-2, m);}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return mod_mul(a, mminvprime(b, m), m);}
 class Solution {
-public:
-    auto z_algo(string str){
-        ll len = str.size();
-        ll l = 0, r = 0;
-        vector<ll> z(len);
-        for(ll i=1;i<len;i++){
-            if(i>r){
-                l=r=i;
-                while(r<len && str[r]==str[r-l]) r++;
-                z[i] = r-l;
-                r--;
-            } else {
-                ll idx = i-l;
-                if(i + z[idx] <= r){
-                    z[i] = z[idx];
-                } else {
-                    l = i;
-                    while(r<len && str[r]==str[r-l]) r++;
-                    z[i] = r-l;
-                    r--;
-                }
+    struct Hashing{
+        string s;
+        ll p = 30;
+        ll m = 1e9 + 7;
+        int n;
+        vector<ll> prefixHash;
+        vector<ll> powersOfP;
+        vector<ll> inversePowersOfP;
+        Hashing(string queryString){
+            s = queryString;
+            n = s.size();
+            prefixHash.resize(n);
+            powersOfP.resize(n);
+            inversePowersOfP.resize(n);
+            calculatePowersAndInversePowersOfP();
+            calculatePrefixHashes();
+        }
+        void calculatePowersAndInversePowersOfP(){ // O(n)
+            ll currentPower = 1;
+            for(int i = 0; i < n; i++){
+                powersOfP[i] = currentPower;
+                currentPower = (currentPower * p) % m;
+            }
+            inversePowersOfP[n - 1] = mminvprime(powersOfP[n - 1], m);
+            for(int i = n - 2; i >= 0; i--){
+                inversePowersOfP[i] = (inversePowersOfP[i + 1] * p) % m;
             }
         }
-        return z;
-    }
+        void calculatePrefixHashes(){ // O(n)
+            ll hashSoFar = 0;
+            for(int i = 0; i < n; i++){
+                hashSoFar = (hashSoFar + (s[i] - 'a' + 1) * powersOfP[i]) % m;
+                prefixHash[i] = hashSoFar;
+            }
+        }
+        
+        
+        ll substringHash(int l, int r){  // O(1)
+            ll val1 = prefixHash[r];
+            ll val2 = l > 0 ? prefixHash[l - 1] : 0LL;
+            return ((val1 - val2 + m) * inversePowersOfP[l]) % m;
+        }
+    };
+    public:
     long long sumScores(string s) {
-        auto z = z_algo(s);
-        z[0] = s.size();
-        ll ans = 0;
-        for(auto x : z) ans+=x;
+        ll n = s.size();
+        Hashing hs = Hashing(s);
+        vector<ll> z(n, 0);
+        z[0] = n;
+        for(ll r = n-1; r>0; r--){
+            
+            ll low = r; 
+            ll high = n-1;
+            while(low <= high){
+                ll mid = (low + high)/2;
 
+                ll hashright = hs.substringHash(r, mid);
+                ll hashleft = hs.substringHash(0, mid-r);
+                
+                if(hashleft == hashright){
+                    z[r] = max(z[r], mid-r+1);
+                    low = mid+1;
+                } else {
+                    high = mid-1;
+                }
+
+            }
+        }
+
+        ll ans = 0;
+        for(auto x : z){
+            ans += x;
+        }
         return ans;
+
     }
 };
