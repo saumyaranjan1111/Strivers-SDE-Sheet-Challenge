d4a387ba34957ff29de2cfbf9a41eac134b29670
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2024-03-30 09:57:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (38.93%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -9,52 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
-//  Morris Inorder Traversal
 class Solution {
 public:
-    TreeNode* findPredecessor(TreeNode* curr){
-        TreeNode* pred = curr->left;
-        while(pred->right != NULL && pred->right != curr){
-            pred = pred->right;
-        }
-        return pred;
+    void solve(TreeNode* root, vector<int> &ans){
+        if(root == NULL) return;
+
+        solve(root->left, ans);
+        ans.push_back(root->val);
+        solve(root->right, ans);
     }
     vector<int> inorderTraversal(TreeNode* root) {
-        TreeNode* curr = root;
         vector<int> ans;
-        
-        while(curr != NULL){
-            if(curr->left != NULL){
-                TreeNode* pred = findPredecessor(curr);
-                if(pred -> right == NULL){
-                    // no link is present 
-                    // create a link
-                    pred->right = curr;
-                    // go to the left
-                    curr = curr->left;
-                }
-                else {
-                    // link is already present
-                    // which means we have already processed all the nodes present in the left subtree
-                    // process the current node and go to the right after removing the link
-                    ans.push_back(curr->val);
-                    pred->right = NULL;
-                    curr = curr->right;
-                }
-            }
-            else{
-                // there is no left node for this node
-                // process this node
-                ans.push_back(curr->val);
 
-                // go to the right, since the left node was not present and have already processed the center node (l n R), so now we need to go to the right node
-
-                curr = curr->right;
-            }
-        }
-
+        solve(root, ans);
         return ans;
     }
 };
