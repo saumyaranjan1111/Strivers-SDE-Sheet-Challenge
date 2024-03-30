6cf74a12fa4001f0c73249d21dbafdcd83589757
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2024-03-30 16:44:23 +0530

    Time: 16 ms (16.83%) | Memory: 21.7 MB (43.48%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,33 +11,22 @@
  */
 class Solution {
 public:
-    int solve(TreeNode* root){
-        if(root == NULL){
-            // height of null tree is zero
-            return 0;
-        }
+    int depth(TreeNode* root, bool &flag){
+        if(root == NULL) return 0;
 
-        int left_height = solve(root->left);
-        int right_height = solve(root->right);
+        int left = depth(root->left, flag);
+        int right = depth(root->right, flag);
 
-        if(left_height == -1 || right_height == -1){
-            // if any one of the children are not balanced then this also cant be balanced
-            return -1;
-        }
-        if(abs(left_height - right_height) > 1){
-            // not a balanced binary tree
-            return -1;
-        }
+        if(abs(left - right) > 1) flag = false;
 
-        // if both the children are balanced and the height difference is also not > 1, then this node is a balanced binary tree, return the height of this tree
-        return max(left_height, right_height) + 1;
-
+        return 1 + max(left, right);
     }
     bool isBalanced(TreeNode* root) {
-        
-        if(solve(root)!=-1)
-        return true;
-        else return false;
+        if(root == NULL) return true;
 
+        bool flag = true;
+        
+        int temp = depth(root, flag);
+        return flag;
     }
 };
