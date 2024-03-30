8bf6673055dc763e1804b1e9b71e12204d55e0d0
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2024-03-30 14:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (30.92%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -12,29 +12,31 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        if(root == NULL) return {};
-        // using two stacks
-        stack<TreeNode*> st1, st2;
-        st1.push(root);
-
-        while(!st1.empty()){
-            auto base_node = st1.top();
-            st1.pop();
-            st2.push(base_node);
-
-            if(base_node->left != NULL){
-                st1.push(base_node->left);
+        // using one stack : just go left until you cant, then try to go right one step, then go all the way left again
+        // when you cant go right from a node, the fact that we attempted to go right from the node indicates that we have already gone through all the nodes in the left subtree of the node, and if the right node is null or already visited, then we process the root node, since both left and right have been now completed
+        stack<pair<TreeNode*, int>> st;
+        vector<int> post;
+        if(root == NULL) return post;
+        st.push({root, 1});
+        while(!st.empty()){
+            auto temp = st.top();
+            st.pop();
+            auto node = temp.first;
+            int n = temp.second;
+            if(n == 1){
+                st.push({node, 2});
+                if(node->left != NULL){
+                    st.push({node->left, 1});
+                }
+            } else if(n == 2){
+                st.push({node, 3});
+                if(node->right != NULL){
+                    st.push({node->right, 1});
+                }
+            } else {
+                post.push_back(node->val);
             }
-            if(base_node->right != NULL){
-                st1.push(base_node->right);
-            }
-        }
-
-        vector<int> ans;
-        while(!st2.empty()){
-            ans.push_back(st2.top()->val);
-            st2.pop();
-        }
-        return ans;
+        } 
+        return post;
     }
 };
