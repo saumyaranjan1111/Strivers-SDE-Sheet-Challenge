5598da344341463c87334756d8285b4dcef6a23f
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2024-03-31 19:48:57 +0530

    Time: 35 ms (5.49%) | Memory: 40 MB (5.03%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> pre(n), post(n);
+        vector<int> pre(n);
         pre[0] = nums[0];
-        post[n-1] = nums[n-1];
+        for(int i = 1; i<n; i++) pre[i] = nums[i] * pre[i-1];
 
-        for(int i = 1; i<n; i++){
-            pre[i] = pre[i-1]*nums[i];
-        }
-        for(int i = n-2; i>=0; i--){
-            post[i] = post[i+1]*nums[i];
-        }
+        vector<int> post(n);
+        post[n-1] = nums[n-1];
+        for(int i = n-2; i>=0; i--) post[i] = nums[i] * post[i+1];
 
         vector<int> ans(n);
         ans[0] = post[1];
         ans[n-1] = pre[n-2];
-
         for(int i = 1; i<n-1; i++){
             ans[i] = pre[i-1]*post[i+1];
         }
-
         return ans;
     }
 };
