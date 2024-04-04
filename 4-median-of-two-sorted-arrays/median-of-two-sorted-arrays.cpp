5ded1938b8dc6e31bc9f50229c1c864be40d089a
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2024-04-04 17:57:49 +0530

    Time: 38 ms (9.57%) | Memory: 96.2 MB (8.91%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,183 +1,41 @@
+using ll = long long;
 class Solution {
 public:
-    int nossmallerorequal(int x, int idx, int arr, vector<int>& nums1, vector<int> &nums2){
-        int result = 0;
-        result += idx+1;
-        if(arr == 1){
-            result += lower_bound(nums1.begin(), nums1.end(), x) - nums1.begin();
+    void shuffle(priority_queue<int> &left, priority_queue<int, vector<int>, greater<int>> &right){
+        while(left.size() > right.size() + 1){
+            ll num = left.top(); 
+            left.pop();
+            right.push(num);
         }
-        else {
-            result += lower_bound(nums2.begin(), nums2.end(), x) - nums2.begin();
+        while(right.size() > left.size()){
+            ll num = right.top();
+            right.pop();
+            left.push(num);
         }
-
-        return result;
-    }
-    double oddcase(vector<int>& nums1, vector<int>& nums2){
-        int n = nums1.size();
-        int m = nums2.size();
-
-        // nums1
-        int low = 0, high = n-1;
-        int mid = low + (high - low)/2;
-        pair<int, int> ans1 = {nums1[mid], 0};
-        while(low <= high){
-            mid = low + (high - low)/2;
-            int smallernums = nossmallerorequal(nums1[mid], mid, 2, nums1, nums2);
-            if(smallernums < (m+n)/2 + 1){
-                // this no lies to the left of median
-                low = mid+1;
-            }
-            else {
-                ans1 = {nums1[mid], smallernums};
-                high = mid-1;
-            }
-        }
-
-        // nums2
-        low = 0, high = m-1;
-        mid = low + (high - low)/2;
-        pair<int, int> ans2 = {nums2[mid], 0};
-        while(low <= high){
-            mid = low + (high - low)/2;
-            int smallernums = nossmallerorequal(nums2[mid], mid, 1, nums1, nums2);
-            if(smallernums < (m+n)/2 + 1){
-                low = mid+1;
-            }
-            else {
-                ans2 = {nums2[mid], smallernums};
-                high = mid-1;
-            }
-        }
-
-        // cout<<ans1.second<<endl;
-        // cout<<ans2.second<<endl;
-
-        if(ans1.second == (m+n)/2 + 1)
-        return ans1.first;
-
-        if(ans2.second == (m+n)/2 + 1)
-        return ans2.first;
-
-        return -1;
-    }
-
-    double evencase(vector<int>& nums1, vector<int>& nums2){
-        int n = nums1.size(), m = nums2.size();
-
-        // search for a number in nums1 and nums2 such that the no of elements <= that no in both the arrays combined is == (m+n)/2
-
-        int low = 0, high = n-1;
-        int mid = low + (high - low)/2;
-        int ans1 = nums1[mid];
-        bool flag = false;
-        while(low <= high){
-            mid = low + (high - low)/2;
-            int ele = nums1[mid];
-
-            int smallerorequal = nossmallerorequal(nums1[mid], mid, 2, nums1, nums2);
-            if(smallerorequal == (m+n)/2){
-                flag = true;
-                ans1 = nums1[mid];
-                break;
-            } 
-            else if(smallerorequal < (m+n)/2){
-                low = mid+1;
-            }
-            else {
-                high = mid-1;
-            }
-        }
-
-        if(!flag){
-            low = 0, high = m-1;
-            mid = low + (high - low)/2;
-            ans1 = nums2[mid];
-            while(low <= high){
-                mid = low + (high - low)/2;
-                int ele = nums2[mid];
-
-                int smallerorequal = nossmallerorequal(nums2[mid], mid, 1, nums1, nums2);
-                if(smallerorequal == (m+n)/2){
-                    ans1 = nums2[mid];
-                    break;
-                } 
-                else if(smallerorequal < (m+n)/2){
-                    low = mid+1;
-                }
-                else {
-                    high = mid-1;
-                }
-            }
-        }
-
-        // (n+m)/2 + 1th number
-        low = 0, high = n-1;
-        mid = low + (high - low)/2;
-        int ans2 = nums1[mid];
-        flag = false;
-        while(low <= high){
-            mid = low + (high - low)/2;
-            int ele = nums1[mid];
-
-            int smallerorequal = nossmallerorequal(nums1[mid], mid, 2, nums1, nums2);
-            if(smallerorequal == (m+n)/2 + 1){
-                flag = true;
-                ans2 = nums1[mid];
-                break;
-            } 
-            else if(smallerorequal < (m+n)/2 + 1){
-                low = mid+1;
-            }
-            else {
-                high = mid-1;
-            }
-        }
-
-        if(!flag){
-            low = 0, high = m-1;
-            mid = low + (high - low)/2;
-            ans2 = nums2[mid];
-            while(low <= high){
-                mid = low + (high - low)/2;
-                int ele = nums2[mid];
-
-                int smallerorequal = nossmallerorequal(nums2[mid], mid, 1, nums1, nums2);
-                if(smallerorequal == (m+n)/2 + 1){
-                    ans2 = nums2[mid];
-                    break;
-                } 
-                else if(smallerorequal < (m+n)/2 + 1){
-                    low = mid+1;
-                }
-                else {
-                    high = mid-1;
-                }
-            }
-        }
-
-        return ((double)ans1 + (double)ans2)/2;
-
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size(), m = nums2.size();
+        // right = minheap, left = maxheap
+        priority_queue <int, vector<int>, greater<int> > right;
+        priority_queue <int> left; 
 
-        if(n == 0){
-            if((m+n)%2 == 0){
-                return ((double)nums2[m/2] + (double)nums2[m/2 - 1])/2;
+        for(int i = 0; i<nums1.size(); i++){
+            if(!right.empty() && right.top() < nums1[i]){
+                right.push(nums1[i]);
+            } else {
+                left.push(nums1[i]);
             }
-            else return (double)nums2[m/2];
+            shuffle(left, right);
         }
-        else if(m == 0){
-            if((m+n)%2 == 0){
-                return ((double)nums1[n/2] + (double)nums1[n/2 - 1])/2;
+
+        for(int i = 0; i<nums2.size(); i++){
+            if(!right.empty() && right.top() < nums2[i]){
+                right.push(nums2[i]);
+            } else {
+                left.push(nums2[i]);
             }
-            else return (double)nums1[n/2];
+            shuffle(left, right);
         }
-
-        if((n+m) %2 != 0){
-            return oddcase(nums1, nums2);
-        }
-
-        return evencase(nums1, nums2);
+        double ans = left.size() != right.size() ? (double)left.top() : ((double)left.top() + (double)right.top())/2;
+        return ans;
     }
 };
