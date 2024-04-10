262950ae4111e2e04045bd80ab352cb5f68a2e92
Saumya Ranjan <80817095+saumyaranjan1111@users.noreply.github.com>
2024-04-10 07:50:28 +0530

    Time: 6 ms (31.96%) | Memory: 14.7 MB (72.10%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -3,36 +3,23 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         ll n = nums.size();
-        ll low = 0, high = n-1;
-        ll ans = high;
-        while(low <=high){
-            ll mid = low + (high-low)/2;
-            if(nums[mid] > nums[n-1]){
-                low = mid+1;
-            } else if(nums[mid] < nums[n-1]){
-                ans = mid;
-                high = mid-1;
+        ll mini = INT_MAX;
+        ll left = 0, right = n-1;
+        while(left <= right){
+            ll mid = left + (right-left)/2;
+            mini = min(mini, (ll)nums[mid]);
+            if(nums[left] == nums[mid] && nums[mid] == nums[right]){
+                left++;
             } else {
-                // nums[mid] == nums[n-1]
-                // whether to go left or to right
-                if(nums[0] > nums[n-1]){
-                    ans = mid;
-                    high = mid-1;
-                } else if(nums[0] < nums[n-1]){
-                    ans = 0;
-                    high = -1;
-                } else {
-                    while(low <= mid && nums[low] >= nums[mid]) {
-                        low++;
-                    }
-                    if(low > high) break;
-                    if(nums[low] < nums[mid]){
-                        ans = low;
-                        high = mid-1;
-                    }
-                }
+                if(nums[mid] >= nums[left]){
+                    mini = min(mini, (ll)nums[left]);
+                    left = mid+1;
+                } else if(nums[mid] <= nums[right]){
+                    mini = min(mini, (ll)nums[mid]);
+                    right = mid-1;
+                }   
             }
         }
-        return nums[ans];
+        return mini;
     }
 };
